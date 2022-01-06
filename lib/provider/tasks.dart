edcda6f85eded2bce2be84a8b290a455ade71931
Abhilash BK <bkabhilash0@gmail.com>
2022-01-06 23:35:21 +0530

    Added Http Fetching

--- lib/provider/tasks.dart
+++ lib/provider/tasks.dart
@@ -1,14 +1,17 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import "package:http/http.dart" as http;
 import './task.dart';
 
 class TasksProvider with ChangeNotifier {
-  final List<Task> _myTasks = [
-    Task(id: "1", title: "Go to Bed 😴", dateTime: DateTime.now()),
-    Task(id: "2", title: "Prepare for Exams 😥", dateTime: DateTime.now()),
-    Task(id: "3", title: "Go Shopping 🛍️", dateTime: DateTime.now()),
-    Task(id: "4", title: "Do Programming 😍", dateTime: DateTime.now()),
-    Task(id: "5", title: "Teach Students 🧑‍🏫", dateTime: DateTime.now()),
-    Task(id: "6", title: "Have a Nice Day! ❤️", dateTime: DateTime.now()),
+  List<Task> _myTasks = [
+    // Task(id: "1", title: "Go to Bed 😴", dateTime: DateTime.now()),
+    // Task(id: "2", title: "Prepare for Exams 😥", dateTime: DateTime.now()),
+    // Task(id: "3", title: "Go Shopping 🛍️", dateTime: DateTime.now()),
+    // Task(id: "4", title: "Do Programming 😍", dateTime: DateTime.now()),
+    // Task(id: "5", title: "Teach Students 🧑‍🏫", dateTime: DateTime.now()),
+    // Task(id: "6", title: "Have a Nice Day! ❤️", dateTime: DateTime.now()),
   ];
 
   List<Task> get items {
@@ -31,6 +34,22 @@ class TasksProvider with ChangeNotifier {
     _myTasks
         .add(Task(title: title, id: DateTime.now().toString(), dateTime: date));
     notifyListeners();
+  }
+
+  Future<void> fetchTasks() async {
+    try {
+      final todos = await http
+          .get(Uri.parse("https://jsonplaceholder.typicode.com/todos"));
+      final data = json.decode(todos.body) as List;
+      final requiredData = data
+          .map((e) =>
+              Task(id: e["id"].toString(), title: e["title"], dateTime: DateTime.now()))
+          .toList();
+      _myTasks = requiredData;
+    } catch (e) {
+      _myTasks = [];
+      rethrow;
+    }
   }
 
   void removeTask(String id) {
